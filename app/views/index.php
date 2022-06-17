a3467c5a80dc9ed824c121ef45aedfa37757dd0d
Kon Kon Tannu Morote <68218785+morotesovtannu@users.noreply.github.com>
2022-06-17 20:16:50 +0900

    よぽすき用

--- app/views/index.php
+++ app/views/index.php
@@ -25,13 +25,13 @@
         <div class="row">
           <div class="col-sm-6">
             <div class="form-group">
-              <label for="dlkeyInput">DLキー</label>
+              <label for="dlkeyInput">DLキー(必須ではありません)</label>
               <input type="text" class="form-control" id="dleyInput" name="dlkey" placeholder="DLキーを入力...">
             </div>
           </div>
           <div class="col-sm-6">
             <div class="form-group">
-              <label for="delkeyInput">DELキー</label>
+              <label for="delkeyInput">DELキー(必須ではありません)</label>
               <input type="text" class="form-control" id="deleyInput" name="delkey" placeholder="DELキーを入力...">
             </div>
           </div>
